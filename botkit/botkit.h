b8530e0de5251f820c4772d74797ff1da0ebbc51
Dave DeLong <davedelong@me.com>
2016-01-20 21:09:22 -0800

    Empty Xcode project for a Mac framework

---
+++ botkit/botkit.h
@@ -0,0 +1,19 @@
+//
+//  botkit.h
+//  botkit
+//
+//  Created by Dave DeLong on 1/20/16.
+//  Copyright © 2016 Utah iOS & Mac Developers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for botkit.
+FOUNDATION_EXPORT double botkitVersionNumber;
+
+//! Project version string for botkit.
+FOUNDATION_EXPORT const unsigned char botkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <botkit/PublicHeader.h>
+
+
