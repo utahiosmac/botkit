b5eddce2fa0e3f11de18af03cb18ef807d06ce50
Dave DeLong <davedelong@me.com>
2016-01-23 19:49:19 -0700

    WebSocket ping-pongs the connection
    There are some circumstances where the connection my just disappear (such as the computer going to sleep). When it wakes back up, the socket is not restored. The WebSocket wrapper has a ~5 second timer it uses to ping the connection to make sure it's still active. If it doesn't get a pong within 5 seconds, it shuts down the socket, which ultimately results in a state change and a re-initialization of the entire connection.

--- botkit/WebSocket.swift
+++ botkit/WebSocket.swift
@@ -12,14 +12,23 @@ import SocketRocket
 internal class WebSocket: NSObject {
     private var socket: SRWebSocket
     private var socketError: NSError?
+    private var receivedLastPong = true
     
     internal var onEvent: (String -> Void)?
     internal var onClose: (NSError? -> Void)?
     
+    private let timerSource: dispatch_source_t
+    
     init(socketURL: NSURL) {
         socket = SRWebSocket(URL: socketURL)
+        timerSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue())
+        
         super.init()
         socket.delegate = self
+    }
+    
+    deinit {
+        dispatch_source_cancel(timerSource)
     }
     
     func open() {
@@ -28,6 +37,30 @@ internal class WebSocket: NSObject {
 }
 
 extension WebSocket: SRWebSocketDelegate {
+    
+    func webSocketDidOpen(webSocket: SRWebSocket!) {
+        let pingInterval = 5.0
+        let intervalInNSec = Int64(pingInterval * Double(NSEC_PER_SEC))
+        let startTime = dispatch_time(DISPATCH_TIME_NOW, intervalInNSec)
+        
+        dispatch_source_set_timer(timerSource, startTime, UInt64(intervalInNSec), NSEC_PER_SEC / 10)
+        dispatch_source_set_event_handler(timerSource) { [unowned self] in
+            if self.receivedLastPong == false {
+                // we did not receive the last pong
+                self.socket.close()
+            } else {
+                // we got a pong recently
+                // send another ping
+                self.receivedLastPong = false
+                self.socket.sendPing(nil)
+            }
+        }
+        dispatch_resume(timerSource)
+    }
+    
+    func webSocket(webSocket: SRWebSocket!, didReceivePong pongPayload: NSData!) {
+        self.receivedLastPong = true
+    }
     
     func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject!) {
         // messages from the socket can either be NSData or NSString
@@ -45,5 +78,6 @@ extension WebSocket: SRWebSocketDelegate {
     func webSocket(webSocket: SRWebSocket!, didCloseWithCode code: Int, reason: String!, wasClean: Bool) {
         // maybe boo?
         onClose?(socketError)
+        dispatch_suspend(timerSource)
     }
 }
